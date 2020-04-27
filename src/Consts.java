99a642923a1d2e4686a30a031233b9e8e0cf6b8d
zencd <z@z.com>
2020-04-27 20:35:53 +0300

    wip

--- src/Consts.java
+++ src/Consts.java
@@ -9,6 +9,6 @@ public interface Consts {
     int PERLIN_DEFAULT = 300;
     int SEA_LEVEL_DEFAULT = 145;
 
-    //int NUM_THREADS = 1;
-    int NUM_THREADS = 2;
+    int NUM_THREADS = 1;
+    //int NUM_THREADS = 2;
 }
